4a3821ea0c0b07453e355dda815c0b06e3f12f4d
caldric <cvaldric@gmail.com>
2020-07-08 13:13:15 -0400

    Add seed route for issues

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,5 +1,7 @@
 // Dependencies
 const express = require('express');
+const Issue = require('../models/issue.js');
+const seed = require('../models/issueSeed.js');
 
 // Config
 const router = express.Router();
@@ -7,6 +9,13 @@ const router = express.Router();
 // Routes
 router.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+// FOR TESTING PURPOSES
+router.get('/seed', async (req, res) => {
+  await Issue.collection.drop();
+  await Issue.create(seed);
+  res.send('Seeding complete!');
 });
 
 // Export router
