d1eee2c95d5434a65cd9b1f475e33b436bec769a
caldric <cvaldric@gmail.com>
2020-07-09 13:10:33 -0400

    Add first and last name to user schema

--- models/users.js
+++ models/users.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     email: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     password: {type: String, required: true}
   }, {timestamps: true}
 );
