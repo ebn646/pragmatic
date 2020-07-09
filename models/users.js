b52b818ab9d97197507571b744009765d70061d9
caldric <cvaldric@gmail.com>
2020-07-08 20:17:38 -0400

    Create user model and schema

---
+++ models/users.js
@@ -0,0 +1,16 @@
+// Dependency
+const mongoose = require('mongoose');
+
+// Schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+  }, {timestamps: true}
+);
+
+// Model
+const User = mongoose.model('User', userSchema);
+
+// Export
+module.exports = User;
