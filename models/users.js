72ae0b1968bcd3f137e6b83396327e0acf911bd0
caldric <cvaldric@gmail.com>
2020-07-10 03:17:28 -0400

    Remove boards from user schema

--- models/users.js
+++ models/users.js
@@ -9,7 +9,6 @@ const userSchema = new mongoose.Schema(
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     password: {type: String, required: true},
-    boards: [mongoose.ObjectId]
   }, {timestamps: true}
 );
 
