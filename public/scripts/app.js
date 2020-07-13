9faecc71860164d6aae92459eaefeff0a303d8ac
caldric <cvaldric@gmail.com>
2020-07-13 14:52:27 -0400

    Create click listener and handler for board rows

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,16 @@ $(() => {
 	$('.icon').tooltip();
 
 	// Clicking on issue row takes user to show page
-	$('tr').click(event => {
+	$('.issue-row').click(event => {
 		const $row = $(event.currentTarget);
 		const issueId = $row.children('.id').text();
 		window.location.href += `/issues/${issueId}`;
+	});
+
+	// Clicking on board row takes user to issues index page
+	$('.board-row').click(event => {
+		const $row = $(event.currentTarget);
+		const boardKey = $row.children('.key').text();
+		window.location.href += `/_${boardKey}`;
 	});
 });
